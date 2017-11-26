77c4956b2cbb90c1a8e5fc46524ebecb54cc7664
conscientiousCode <conscientiouscode@outlook.com>
2017-11-25 23:05:20 -0800

    Masked over an ArrayList as a cart for easy session information transfer

--- MTG Misprints/src/pageutils/Cart.java
+++ MTG Misprints/src/pageutils/Cart.java
@@ -1,5 +1,17 @@
 package pageutils;
+import java.util.ArrayList;
+import dto.CartItem;
 
-public class Cart {
-
+public class Cart extends ArrayList<CartItem>{
+	
+	public Cart(){
+		super();
+	}
+	public Cart(int initCapacity){
+		super(initCapacity);
+	}
+	
+	public Cart[] toArray(){
+		return super.toArray(new Cart[0]);
+	}
 }
